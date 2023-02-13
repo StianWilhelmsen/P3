e035fafb9f8e0fe3b5909567ced8c87acc9645e0
Stian <stianwil@stud.ntnu.no>
2023-02-13 18:08:46 +0100

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+
+    static class ClientHandler extends Thread {
+
+        public Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String num1String = in.readLine();
+                String num2String = in.readLine();
+                String operator = in.readLine();
+
+                System.out.println("Received values from client: num1=" + num1String + ", num2=" + num2String + ", operator=" + operator);
+
+
+                int num1 = Integer.parseInt(num1String);
+                int num2 = Integer.parseInt(num2String);
+                int result = 0;
+                if (operator.equals("+")) {
+                    result = num1 + num2;
+                } else if (operator.equals("-")) {
+                    result = num1 - num2;
+                } else {
+                    out.println("Invalid Operator");
+                }
+                System.out.println(result);
+                out.println(result);
+                out.close();
+                in.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(999);
+        System.out.println("Server started on port 999");
+
+        while (true) {
+            // Venter på en klient tilkobling
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected");
+
+            // Start en ny tråd for hver klient
+            new ClientHandler(clientSocket).start();
+        }
+
+    }
+}
---
+++ src/client.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class client {
+
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        try {
+            Socket s = new Socket("localhost", 999);
+            System.out.println("Socket connected to server");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+
+            while (true) {
+                System.out.println("Choose your operator: (add/sub):");
+                String operation = scanner.nextLine();
+                System.out.println("Enter first number:");
+                String num1 = scanner.nextLine();
+                System.out.println("Enter second number:");
+                String num2 = scanner.nextLine();
+
+                out.println(num1);
+                out.println(num2);
+                out.println(operation);
+
+                String result = in.readLine();
+                System.out.println("Result: " + result);
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
